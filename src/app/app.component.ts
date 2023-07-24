34059f96b37306e0f4df4d71649c1b1fb1e6f078
Tanassar Hussain <trhussain@cinnova.com>
2023-07-24 11:11:07 +0500

    Get User Profile_v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent implements OnInit {
   // New Way To Inject Dependencies instead of using the Construtor
   private msalService = inject(MsalService);
   private http = inject(HttpClient);
+  userProfilePicture: string | undefined;
 
   
   ngOnInit(): void {
@@ -56,23 +57,41 @@ export class AppComponent implements OnInit {
 
   async getUserProfile() {
     const activeAccount = this.msalService.instance.getActiveAccount();
-
+  
     if (activeAccount) {
       try {
         const tokenResponse = await this.msalService.instance.acquireTokenSilent({
           account: activeAccount,
-          scopes: ['user.read']
+          scopes: ['user.read', 'User.ReadBasic.All'] // Add 'User.ReadBasic.All' scope to access the profile picture
         });
-
+  
         const headers = new HttpHeaders({
           Authorization: `Bearer ${tokenResponse.accessToken}`
         });
-
+  
         // Call Microsoft Graph API to get user profile
         this.http.get('https://graph.microsoft.com/v1.0/me', { headers }).subscribe(
           (response) => {
             this.userProfile = response;
             console.log('User Profile:', this.userProfile);
+  
+            // Now fetch the profile picture
+            this.http.get('https://graph.microsoft.com/v1.0/me/photo/$value', {
+              headers: headers,
+              responseType: 'blob' // Set the response type to 'blob' to handle binary data
+            }).subscribe(
+              (pictureBlob) => {
+                // Convert the blob data to a URL for displaying in the UI
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                  this.userProfilePicture = reader.result as string;
+                };
+                reader.readAsDataURL(pictureBlob);
+              },
+              (error) => {
+                console.log('Error fetching user profile picture:', error);
+              }
+            );
           },
           (error) => {
             console.log('Error fetching user profile:', error);
@@ -84,4 +103,7 @@ export class AppComponent implements OnInit {
       }
     }
   }
+  
+
 }
+
